25e256703a708641d0bff8721144b14b766e26d7
pinki patel <pinkipatel3383@gmail.com>
2024-10-05 00:26:53 +0530

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,52 @@
+import Dosa from "../images/1.jpg";
+import Chola from "../images/2.jpeg";
+import Idli from "../images/3.webp";
+import MasalaDosa from "../images/4.jpg";
+import Paneer from "../images/paneer.jpg";
+import Gujrati from "../images/6.png";
+
+export const MenuList = [
+  {
+    name: "Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Dosa,
+    price: 200,
+  },
+  {
+    name: "Chola",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Chola,
+    price: 250,
+  },
+  {
+    name: "Idli Sambhar",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Idli,
+    price: 300,
+  },
+  {
+    name: "Masala Dosa",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: MasalaDosa,
+    price: 100,
+  },
+  {
+    name: "Paneer",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Paneer,
+    price: 400,
+  },
+  {
+    name: "Gujrati",
+    description:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat, ab!",
+    image: Gujrati,
+    price: 500,
+  },
+  
+];
